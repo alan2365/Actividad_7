27e265bea145583ea05753313a6c7c8d58a0223e
Alan <alaneduardo3@hotmail.com>
2021-10-07 22:35:42 -0500

    Actividad 7 de clase computacion en java

---
+++ src/main/java/com/mycompany/actividad_7_cj/Actividad.java
@@ -0,0 +1,60 @@
+
+package com.mycompany.actividad_7_cj;
+
+/**
+ *
+ * @author AlanEduardo
+ */
+public class Actividad {
+
+    public static void main(String[] args) {
+        
+        sucesionFibonacci(20);
+        primo(20);
+    }
+    
+    
+     public static void sucesionFibonacci (int serie) {
+        int num1 = 0, num2 = 1, suma = 1;
+ 
+        System.out.println("Sucesión de Fibonacci:");
+         
+        for (int i = 1; i < serie; i++) {
+             
+            System.out.print(suma + ", ");
+             
+            suma = num1 + num2;
+            num1 = num2;
+            num2 = suma;
+        }
+    }
+
+    public static void primo(int numero) {
+        
+        int[] arreglo = new int[numero];
+        int n = 2;
+        int total = 0;
+
+        while (total < numero) {
+
+            boolean esPrimo = true;
+
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+            if (esPrimo) {
+                arreglo[total] = n;
+                total++;
+            }
+            n++;
+        }
+        System.out.println("\n\nNumeros Primos:");
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.print(arreglo[i] + ", ");
+        }
+    }
+
+}
